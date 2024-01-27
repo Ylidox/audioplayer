f3b816ee5534648a551ac501dfc1d4d2dbd35cce
Ylidox <artiomaliev2017@gmail.com>
2024-01-27 20:23:48 +0300

    backup db

--- back/db.sql
+++ back/db.sql
@@ -2,13 +2,13 @@ create table customer(
     id serial primary key,
     login text not null,
     password text not null
-)
+);
 
 create table playlist(
     id serial primary key,
     customer_id  int references public.customer(id),
     name text not null
-)
+);
 
 create table musik(
     id serial primary key,
@@ -17,9 +17,8 @@ create table musik(
     path text not null,
     name text not null,
     author text,
-    liked boolean NOT NULL DEFAULT FALSE,
-    count_listenings int DEFAULT 0
-)
+    liked boolean NOT NULL DEFAULT FALSE
+);
 
 ALTER TABLE musik
 ADD liked boolean NOT NULL DEFAULT FALSE;
